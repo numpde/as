682177c7ca34a6cec84d1afa5e3fe8b500a08b2f
r <romanize@hotmail.com>
2017-10-26 18:59:01 +0800

    RA: Unnormalized and normalized; all eigenvectors

--- p/network/20171002-RatPathways-EPFL/C-graph1/step3-1-eig.m
+++ p/network/20171002-RatPathways-EPFL/C-graph1/step3-1-eig.m
@@ -1,6 +1,8 @@
 
 % RA, 2017-10-26
 
+%%
+
 % INPUT
 input_filename_mat  = './OUTPUT/UV/column-d-laplacian.mat';
 
@@ -8,18 +10,38 @@ input_filename_mat  = './OUTPUT/UV/column-d-laplacian.mat';
 number_of_eigenvectors = 5;
 
 % OUTPUT
-output_filename_mat = './OUTPUT/UV/column-e-lapeig.mat';
+output_filename_unnorm_mat = './OUTPUT/UV/column-e-lapeig-unnorm.mat';
+output_filename_normal_mat = './OUTPUT/UV/column-e-lapeig-normal.mat';
 
+%%
 
 % 1. Load the Laplacian
 L = load(input_filename_mat); 
 L = L.L;
 
-% 2. Compute first few eigenvalues
-[V, E] = eig(full(L));
+%%
 
-V = V(:, 1:number_of_eigenvectors);
+% 2.1.a Compute the eigenvalues (unnormalized)
+[V, E] = eig(full(L));
 E = diag(E);
 
-% 3. Save
-save(output_filename_mat, 'L', 'V', 'E', '-v7.3');
+% 2.1.b Save
+save(output_filename_unnorm_mat, 'L', 'V', 'E', '-v7.3');
+clear V E
+
+%%
+
+% 2.2.a Compute the eigenvalues (normalized)
+d = sparse(diag(sqrt(diag(L))));
+D = d * d;
+[U, E] = eig(full(L), full(D));
+W = d * U;
+E = diag(E);
+
+% U are eigenvectors of (D \ L)
+% W are eigenvectors of (d \ L / d)
+
+% 2.2.b Save
+save(output_filename_normal_mat, 'L', 'U', 'W', 'E', '-v7.3');
+clear U W E
+
