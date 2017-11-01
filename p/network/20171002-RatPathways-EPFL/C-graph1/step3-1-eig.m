54ffde071c490d61dcdc9d5ec9c73e9189fa7f94
numpde <romanize@hotmail.com>
2017-11-01 14:39:07 +0800

    RA: 33 eigenvectors

--- p/network/20171002-RatPathways-EPFL/C-graph1/step3-1-eig.m
+++ p/network/20171002-RatPathways-EPFL/C-graph1/step3-1-eig.m
@@ -7,7 +7,7 @@
 input_filename_mat  = './OUTPUT/UV/column-d-laplacian.mat';
 
 % PARAMETERS
-number_of_eigenvectors = 5;
+number_of_eigenvectors = 33;
 
 % OUTPUT
 output_filename_unnorm_mat = './OUTPUT/UV/column-e-lapeig-unnorm.mat';
@@ -19,14 +19,19 @@ output_filename_normal_mat = './OUTPUT/UV/column-e-lapeig-normal.mat';
 L = load(input_filename_mat); 
 L = L.L;
 
+% % DEBUG
+% warning('DEBUG MODE. FAKE MATRIX.');
+% L = randn(number_of_eigenvectors * 2);
+
 %%
 
 % 2.1.a Compute the eigenvalues (unnormalized)
 [V, E] = eig(full(L));
+V = V(:, [1 : number_of_eigenvectors]);
 E = diag(E);
 
 % 2.1.b Save
-save(output_filename_unnorm_mat, 'L', 'V', 'E', '-v7.3');
+save(output_filename_unnorm_mat, '-v7', 'L', 'V', 'E', 'number_of_eigenvectors');
 clear V E
 
 %%
@@ -35,6 +40,7 @@ clear V E
 d = sparse(diag(sqrt(diag(L))));
 D = d * d;
 [U, E] = eig(full(L), full(D));
+U = U(:, [1 : number_of_eigenvectors]);
 W = d * U;
 E = diag(E);
 
@@ -42,6 +48,6 @@ E = diag(E);
 % W are eigenvectors of (d \ L / d)
 
 % 2.2.b Save
-save(output_filename_normal_mat, 'L', 'U', 'W', 'E', '-v7.3');
+save(output_filename_normal_mat, '-v7', 'L', 'U', 'W', 'E', 'number_of_eigenvectors');
 clear U W E
 
