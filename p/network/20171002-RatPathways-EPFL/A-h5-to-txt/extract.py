dc5e9527a1336d11383fe27e6b920ff0494f49b6
r <romanize@hotmail.com>
2017-11-02 12:08:52 +0800

    RA: Extract also the XYZ location of the neurons

--- p/network/20171002-RatPathways-EPFL/A-h5-to-txt/extract.py
+++ p/network/20171002-RatPathways-EPFL/A-h5-to-txt/extract.py
@@ -5,10 +5,10 @@
 filename_input = "./ORIGINALS/UV/pathways_mc0_Column.h5"
 
 # Output:
-output_dir = "./OUTPUT"
-output_filename_mat      = output_dir + "/UV/pathways_mc0_Column.h5.mat"
-output_filename_rat_head = output_dir + "/UV/ratcolumn_head.txt"
-output_filename_rat_data = output_dir + "/UV/ratcolumn_data.txt"
+output_dir = "./OUTPUT/"
+output_filename_mat      = output_dir + "UV/pathways_mc0_Column.h5.mat"
+output_filename_rat_head = output_dir + "UV/ratcolumn_head.txt"
+output_filename_rat_data = output_dir + "UV/ratcolumn_data.txt"
 
 import h5py
 import numpy as np
@@ -20,7 +20,22 @@ fp = f["populations"]
 #for n in fc : print(n)
 #for n in fp : print(n)
 
-# 1. Construct the graph adjacency matrix
+# 0. Sanity checks
+
+# Check that the order of layers is the same
+assert(list(fc.keys()) == list(fp.keys()))
+
+# 1. Get the "physical" locations of the neurons
+
+XYZ = []
+for (k0, v0) in fp.items() :
+	XYZ.append(np.vstack(v0['locations']))
+
+# Location matrix of size N x 3
+# where N is the number of neurons
+XYZ = np.vstack(XYZ)
+
+# 2. Construct the graph adjacency matrix
 
 M = []
 for (k0, v0) in fc.items() :
@@ -37,16 +52,19 @@ for (k0, v0) in fc.items() :
 # This is the adjacency matrix (i -> j)
 M = np.vstack(M)
 
+# Convert to a sparse matrix
+from scipy.sparse import csc_matrix
+M = csc_matrix(M)
+
 #plt.ion(); plt.imshow(M); plt.show(); input("# Press enter...")
 
 
-# 2a. Save in Matlab format
+# 3a. Save the adjacency matrix in Matlab format
 
-from scipy.sparse import csc_matrix
 from scipy.io import savemat
-savemat(output_filename_mat, {'M' : csc_matrix(M)}, do_compression=True)
+savemat(output_filename_mat, {'M' : M, 'XYZ' : XYZ}, do_compression=True)
 
-# 2b. Output in txt format
+# 3b. Save the adjacency in txt format
 
 (I, J) = np.nonzero(M)
 IJ = list(zip(list(I), list(J)))
@@ -54,14 +72,13 @@ IJ = list(zip(list(I), list(J)))
 base = 0
 
 with open(output_filename_rat_head, "w") as f :
-    print("# Connections from {}".format(filename_input), file=f)
-    print("# Adjacency matrix size: {}".format(M.shape), file=f)
-    print("# Number of connections: {}".format(len(IJ)), file=f)
-    print("# Array base / first node ID: {}".format(base), file=f)
-    print("# FromNodeId\tToNodeId", file=f)
+	print("# Connections from {}".format(filename_input), file=f)
+	print("# Adjacency matrix size: {}".format(M.shape), file=f)
+	print("# Number of connections: {}".format(len(IJ)), file=f)
+	print("# Array base / first node ID: {}".format(base), file=f)
+	print("# FromNodeId\tToNodeId", file=f)
 
 with open(output_filename_rat_data, "w") as f :
-    for (i, j) in IJ :
-            print("{}\t{}".format(base+i, base+j), file=f)
-
+	for (i, j) in IJ :
+		print("{}\t{}".format(base+i, base+j), file=f)
 
