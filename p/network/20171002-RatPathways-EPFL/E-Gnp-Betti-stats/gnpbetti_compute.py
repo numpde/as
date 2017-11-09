e7f58fd9594ba676a627d305e32704512a552c6e
r <romanize@hotmail.com>
2017-11-09 18:12:57 +0800

    RA: Statistics of Betti numbers for Erdos-Renyi random graphs

---
+++ p/network/20171002-RatPathways-EPFL/E-Gnp-Betti-stats/gnpbetti_compute.py
@@ -0,0 +1,75 @@
+
+# RA, 2017-11-04
+
+# Statistics for Betti numbers of the random G_{n,p} graph
+
+import networkx as nx
+import numpy    as np
+
+import progressbar, time
+
+from topology_localcopy import betti_bin as BETTI
+
+# INPUT ---- #
+
+# The highest Betti number rank that might occur
+max_expected_betti = 10
+# Number of nodes in the random graph
+nn = [10, 20, 30]
+# Maximal number of runs for the statistic
+max_runs = 1000
+# Maximal time allowance for each p (in seconds)
+max_time_per_p = 100
+# Range of edge proba in G_{n,p}
+P = [p/100 for p in range(1, 77)]
+
+# OUTPUT --- #
+
+output_file_stats = "gnpbetti-out_n={}.pkl"
+
+# ---------- #
+
+# Computes average and std dev of Betti numbers
+# for the G_{n,p} random graph over (runs) runs
+def betti_av(n, p, runs) :
+	#print("n: {}, p: {}".format(n, p))
+	
+	B = np.zeros( (0, 1 + max_expected_betti) )
+	
+	t0 = time.time()
+	for i in range(0, runs) :
+		G = nx.fast_gnp_random_graph(n, p, seed=i)
+		b = BETTI(nx.find_cliques(G))
+		# Append to B, padding with zeros
+		B = np.vstack([B, b + ([0] * (B.shape[1] - len(b)))])
+		
+		# Terminate if it takes too long
+		if ((time.time() - t0) > max_time_per_p) : break
+
+	return (np.mean(B, 0), np.std(B, 0), i)
+
+def stats(n) :
+	print("Collecting stats for n = {}".format(n))
+	
+	# Compute the Betti numbers as (average, stddev) for each p
+	bar = progressbar.ProgressBar()
+	MS = [betti_av(n, p, max_runs) for p in bar(P)]
+
+	# Separate the average
+	M = [B[0] for B in MS]
+	# Separate the standard deviation
+	S = [B[1] for B in MS]
+	# Separate the number of runs taken
+	R = [B[2] for B in MS]
+
+	# Write output
+	import pickle
+	data = {'M' : M, 'S' : S, 'n' : n, 'P' : P, 'runs' : R}
+	filename = output_file_stats.format(n)
+	print("Writing results to:", filename)
+	pickle.dump(data, open(filename, "wb"))
+
+if __name__ == "__main__":
+	
+	for n in nn : stats(n)
+
---
+++ p/network/20171002-RatPathways-EPFL/E-Gnp-Betti-stats/gnpbetti_plot.py
@@ -0,0 +1,36 @@
+
+# RA, 2017-11-07
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+# INPUT
+input_file_stats = "gnpbetti-out_n={}.pkl"
+
+# OUTPUT
+output_file_plot = "gnpbetti_n={}"
+
+for n in [10, 20, 30] :
+	
+	# Read the statistics
+	data = pickle.load(open(input_file_stats.format(n), "rb"))
+	p = np.asmatrix(data['P']).reshape(-1, 1)
+
+	M = np.asmatrix(data['M'])
+	S = np.asmatrix(data['S'])
+
+	# Number of Betti numbers
+	nb = max(M.nonzero()[1])
+
+	plt.clf()
+	
+	for b in range(0, nb) :
+		plt.errorbar(p, M[:, b], yerr = [M[:, b] - np.maximum(M[:, b] - S[:, b], 0), S[:, b]])
+
+	plt.xlabel("Edge-inclusion probability p for a graph on {} nodes".format(n))
+	plt.ylabel("Betti number")
+
+	plt.legend(["0th", "1st", "2nd", "3rd"] + ["{}th".format(k) for k in range(4, 10)])
+	plt.savefig(output_file_plot.format(n) + ".eps")
+	plt.savefig(output_file_plot.format(n) + ".png")
