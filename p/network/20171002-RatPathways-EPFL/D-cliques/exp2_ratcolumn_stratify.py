9e57839222506fdda96741d32eeb04abc939fce5
r <romanize@hotmail.com>
2017-11-11 23:26:20 +0800

    RA: Clique number for stratified ratcolumn graph (draft)

---
+++ p/network/20171002-RatPathways-EPFL/D-cliques/exp2_ratcolumn_stratify.py
@@ -0,0 +1,65 @@
+
+# RA, 2017-11-11
+
+# Track the number of cliques in the ratcolumn graph
+# as edges between distant nodes are dropped
+
+# INPUT ---- #
+input_file_xyz = "../A-h5-to-txt/OUTPUT/UV/pathways_mc0_Column.h5.mat"
+input_file_graph = "../C-graph1/OUTPUT/UV/column-a-graph.pkl"
+
+# OUTPUT --- #
+pass
+
+# ---------- #
+
+import pickle
+import scipy.io
+import collections
+import numpy as np
+import networkx as nx
+
+G = nx.gnp_random_graph(100, 0.1, seed=0)
+for (a, b, ed) in G.edges(data=True) :
+	ed['d'] = abs(a - b)
+
+# Graph 
+try :
+	G
+except NameError :
+	G = pickle.load(open(input_file_graph, "rb"))['G']
+
+	# Nodes locations in 3D
+	X = scipy.io.loadmat(input_file_xyz)["XYZ"]
+
+	# Compute edge lengths
+	for (a, b, ed) in G.edges(data=True) :
+		ed['d'] = np.linalg.norm(X[a, :] - X[b, :], 2)
+
+maxd = max(ed['d'] for (a, b, ed) in G.edges(data=True))
+
+P = [i/1000 for i in range(0, 1000)]
+
+def cliques(p) :
+	# Maximal allowed edge length
+	d = maxd * p
+	# Stratify graph to short edges
+	g = nx.Graph()
+	g.add_nodes_from(G.nodes())
+	g.add_edges_from((a, b) for (a, b, data) in G.edges(data=True) if (data['d'] < d))
+	# Fraction of edges included
+	fe = g.number_of_edges() / G.number_of_edges()
+	# Count k-cliques
+	nc = dict(collections.Counter(len(c) for c in nx.enumerate_all_cliques(g)))
+	# Convert to a list 
+	nc = [nc.get(k, 0) for k in range(0, 1 + max(nc.keys()))]
+	return (fe, nc)
+
+# TODO: Parallelize
+CLIQUES = [cliques(p) for p in P]
+
+FE = [fe for (fe, _) in CLIQUES]
+NC = [nc for (_, nc) in CLIQUES]
+
+print(FE)
+print(NC)
