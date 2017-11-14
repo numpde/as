e5bc3526cd2f7b06b2b4a6f5cc42ad7217581311
r <romanize@hotmail.com>
2017-11-14 09:40:01 +0800

    RA: Plotting routines for stratification experiment

---
+++ p/network/20171002-RatPathways-EPFL/D-cliques/plot_exp2a.py
@@ -0,0 +1,54 @@
+
+# RA, 2017-11-13
+
+# Plot the results of exp2a
+
+### IMPORTS -- #
+
+import pickle
+import numpy    as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+### INPUT ---- #
+
+input_file_stats = "./OUTPUT/column-stratify-stats-2a-dist.pkl"
+
+### OUTPUT --- #
+
+pass
+
+### MEAT ----- #
+
+# LL is a list of lists
+# Append zeros to each list for uniform length
+def padzeros(LL) :
+	maxL = max(len(L) for L in LL)
+	return [(L + ([0] * (maxL - len(L)))) for L in LL]
+
+# data has keys ['maxd', 'P', 'FE', 'NC']
+data = pickle.load(open(input_file_stats, "rb"))
+
+# We omit 0-cliques
+NC = np.vstack(padzeros(data['NC']))[:, 1:]
+
+
+FE  = np.asarray(data['FE'])
+
+
+plt.plot(FE, NC, '.-')
+
+plt.yscale('log')
+plt.xscale('log')
+
+plt.gca().set_xlim([2e-5, 2])
+#plt.xticks()
+plt.gca().set_ylim([2e-1, 2e8])
+plt.yticks([10**e for e in range(0, 9)])
+
+plt.xlabel("Fraction of shortest egdes kept")
+plt.ylabel("Number of cliques")
+
+plt.legend(["{}-cliques".format(1+i) for i in range(NC.shape[1])], loc='upper left')
+
+plt.show()
---
+++ p/network/20171002-RatPathways-EPFL/D-cliques/plot_exp2b.py
@@ -0,0 +1,54 @@
+
+# RA, 2017-11-14
+
+# Plot the results of exp2b
+
+### IMPORTS -- #
+
+import pickle
+import numpy    as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+### INPUT ---- #
+
+input_file_stats = "./OUTPUT/column-stratify-stats-2b-rand.pkl"
+
+### OUTPUT --- #
+
+pass
+
+### MEAT ----- #
+
+# LL is a list of lists
+# Append zeros to each list for uniform length
+def padzeros(LL) :
+	maxL = max(len(L) for L in LL)
+	return [(L + ([0] * (maxL - len(L)))) for L in LL]
+
+# data has keys ['NCS', 'RUN', 'FE', 'NCM']
+data = pickle.load(open(input_file_stats, "rb"))
+
+# We omit 0-cliques
+NCM = np.vstack(padzeros(data['NCM']))[:, 1:]
+NCS = np.vstack(padzeros(data['NCS']))[:, 1:]
+
+FE  = np.asarray(data['FE'])
+
+for j in range(0, NCM.shape[1]) :
+	plt.errorbar(FE, NCM[:, j], yerr=0*NCS[:, j], fmt='.-')
+
+plt.yscale('log')
+plt.xscale('log')
+
+plt.gca().set_xlim([2e-5, 2])
+#plt.xticks()
+plt.gca().set_ylim([2e-1, 2e8])
+plt.yticks([10**e for e in range(0, 9)])
+
+plt.xlabel("Fraction of random egdes kept")
+plt.ylabel("Average number of cliques")
+
+plt.legend(["{}-cliques".format(1+i) for i in range(NCM.shape[1])], loc='upper left')
+
+plt.show()
