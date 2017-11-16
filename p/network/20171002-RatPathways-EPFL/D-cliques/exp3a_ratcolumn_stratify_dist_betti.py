16438e44928bcf0f24ae35c1026221854a55caf6
r <romanize@hotmail.com>
2017-11-16 19:57:30 +0800

    RA: Stratified (rat column) graph by distance -- Betti numbers (before run)

---
+++ p/network/20171002-RatPathways-EPFL/D-cliques/exp3a_ratcolumn_stratify_dist_betti.py
@@ -0,0 +1,90 @@
+
+# RA, 2017-11-16
+
+# Track the Betti numbers in the ratcolumn graph
+# as edges between distant nodes are dropped
+
+### IMPORTS -- #
+
+import pickle
+import scipy.io
+import progressbar
+import numpy    as np
+import networkx as nx
+
+from joblib   import Parallel, delayed
+from topology import betti_bin_cpp as betti
+
+### INPUT ---- #
+
+input_file_xyz = "../A-h5-to-txt/OUTPUT/UV/pathways_mc0_Column.h5.mat"
+input_file_graph = "../C-graph1/OUTPUT/UV/column-a-graph.pkl"
+
+### OUTPUT --- #
+
+output_file_stats = "./OUTPUT/column-stratify-betti-3a-dist.pkl"
+
+### PARAMS --- #
+
+# Fraction of maximal edge length
+P = np.logspace(-3, 0, 31).tolist()
+
+# Number of computing threads to use in parallel
+num_of_cores = 10
+
+## Use this for testing purposes
+#G = nx.gnp_random_graph(100, 0.4, seed=0)
+#for (a, b, ed) in G.edges(data=True) : ed['d'] = np.random.rand()
+
+### MEAT ----- #
+
+# See if a graph is already provided
+try :
+	G
+# Otherwise load the ratcolumn graph from disk
+except NameError :
+	G = pickle.load(open(input_file_graph, "rb"))['G']
+
+	# Nodes locations in 3D
+	X = scipy.io.loadmat(input_file_xyz)["XYZ"]
+
+	# Compute edge lengths; use euclidean distance
+	for (a, b, ed) in G.edges(data=True) :
+		ed['d'] = np.linalg.norm(X[a, :] - X[b, :], 2)
+
+# Maximal edge length
+maxd = max(ed['d'] for (a, b, ed) in G.edges(data=True))
+
+# Compute Betti numbers when long edges are removed
+#
+# The parameter 0 <= p <= 1 is the cut-off
+# of edge length wrt the longest edge
+#
+def betti_p(p) :
+	# Maximal allowed edge length
+	d = maxd * p
+	# Stratify graph to short edges
+	g = nx.Graph()
+	g.add_nodes_from(G.nodes())
+	g.add_edges_from((a, b) for (a, b, data) in G.edges(data=True) if (data['d'] <= d))
+	# Fraction of edges included
+	fe = g.number_of_edges() / G.number_of_edges()
+	# Compute the Betti numbers
+	be = betti(nx.find_cliques(g))
+	# be[k] is now the k-th Betti number
+	return (fe, be)
+
+def job(p) :
+	return betti_p(p)
+
+progbar = progressbar.ProgressBar()
+BETTI_P = Parallel(n_jobs=num_of_cores)(delayed(job)(p) for p in progbar(P))
+
+FE = [fe for (fe, _) in BETTI_P]
+BE = [be for (_, be) in BETTI_P]
+
+# Collect the results
+results = { "maxd" : maxd, "P" : P, "FE" : FE, "BE" : BE }
+
+# Save to file
+pickle.dump(results, open(output_file_stats, "wb"))
---
+++ p/network/20171002-RatPathways-EPFL/D-cliques/plot_exp3a.py
@@ -0,0 +1,58 @@
+
+# RA, 2017-11-16
+
+# Plot the results of exp3a
+
+### IMPORTS -- #
+
+import pickle
+import numpy    as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+### INPUT ---- #
+
+input_file_stats = "./OUTPUT/column-stratify-betti-3a-dist.pkl"
+
+### OUTPUT --- #
+
+output_file_plot = input_file_stats + ".eps"
+
+### MEAT ----- #
+
+# LL is a list of lists
+# Append zeros to each list for uniform length
+def padzeros(LL) :
+	maxL = max(len(L) for L in LL)
+	return [(L + ([0] * (maxL - len(L)))) for L in LL]
+
+# data has keys ['maxd', 'P', 'FE', 'BE']
+data = pickle.load(open(input_file_stats, "rb"))
+
+# Betti numbers
+BE = np.vstack(padzeros(data['BE']))
+
+
+FE  = np.asarray(data['FE'])
+
+
+plt.plot(FE, BE, '.-')
+
+
+plt.yscale('log')
+plt.xscale('log')
+
+plt.gca().set_xlim([2e-5, 2])
+#plt.xticks()
+
+#plt.gca().set_ylim([2e-1, 2e8])
+#plt.yticks([10**e for e in range(0, 9)])
+
+plt.xlabel("Fraction of shortest egdes kept")
+plt.ylabel("Betti numbers")
+
+plt.legend(["b{}".format(i) for i in range(BE.shape[1])], loc='upper left')
+
+plt.savefig(output_file_plot)
+
+plt.show()
