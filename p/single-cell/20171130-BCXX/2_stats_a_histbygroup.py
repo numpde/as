e3adf59bfdcbba1d2cd33a520e9d9009587d53df
r <romanize@hotmail.com>
2017-12-04 10:03:04 +0800

    RA: Histograms for expression w/ & w/o normalization

--- p/single-cell/20171130-BCXX/2_stats_a_histbygroup.py
+++ p/single-cell/20171130-BCXX/2_stats_a_histbygroup.py
@@ -18,7 +18,7 @@ input_file_selected = "OUTPUT/UV/GSE75688_GEO_processed_Breast_Cancer_raw_TPM_ma
 
 ### OUTPUT --- #
 
-output_file_fig = "OUTPUT/2_stats_b/{}.png" #.format(group)
+output_file_fig = "OUTPUT/2_stats_a/{normalization}_{group}_XX.{extension}"
 
 ### MEAT ----- #
 
@@ -32,22 +32,24 @@ X = data['X']
 # Labels for axis/dimension of data
 (axis_smpl, axis_gene) = (data['axis_smpl'], data['axis_gene'])
 
-#
-(n_samples, n_genes) = (X.shape[axis_smpl], X.shape[axis_gene])
+# Number of samples, number of genes
+#(n_samples, n_genes) = (X.shape[axis_smpl], X.shape[axis_gene])
 
 # BCXX(LN)(_Re)_XX
 header = data['header']
 #print(header)
 
-# BCXX
+# Get the sample groups
+#
+# Option 1: BCXX
 #groups = [n[0:4] for n in header]
-
-# BCXX(LN)(_Re)
+#
+# Option 2: BCXX(LN)(_Re)
 groups = [re.findall("(.*)_[0-9]+", n)[0] for n in header]
 
-# Make unique and sort
-groups = list(sorted(set(groups)))
-print(groups)
+# Make groups unique and sort
+groups = sorted(list(set(groups)))
+print("Groups:", ', '.join(groups))
 
 GROUP = dict()
 for g in groups :
@@ -62,23 +64,55 @@ for g in groups :
 # https://stackoverflow.com/questions/4150171/how-to-create-a-density-plot-in-matplotlib
 from scipy.stats import gaussian_kde
 
-for (g, Y) in Progress()(GROUP.items()) :
-	plt.clf()
-	plt.title("Sample subset: {}_XX ({} samples)".format(g, Y.shape[axis_smpl]))
-	plt.xlabel("log10(expression)")
-	plt.ylabel("Frequency")
+for normalize in [True, False] :
+	print("Computing with normalization flag:", normalize)
+
+	xlabel = ("log10(normalized expression)" if normalize else "log10(expression)")
 	
-	for s in range(Y.shape[axis_smpl]) :
-		x = np.take(Y, s, axis = axis_smpl)
+	TF = dict()
+	for (g, Y) in Progress()(GROUP.items()) :
+		TF[g] = []
+		for s in range(Y.shape[axis_smpl]) :
+			x = np.take(Y, s, axis = axis_smpl)
 		
-		#print(len(x), np.histogram(np.log(x[x != 0])))
-		
-		x = np.log10(x[x != 0])
-		
-		t = np.linspace(min(x), max(x), 100)
-		f = gaussian_kde(x)(t)
-		
-		plt.plot(t, f)
+			x = x[x != 0]
 
-	plt.savefig(output_file_fig.format(g))
+			d = dict()
+			d['median'] = np.median(x)
+			
+			if normalize : x = x / np.sum(x)
+			
+			x = np.log10(x)
+			
+			t = np.linspace(min(x), max(x), 100)
+			f = gaussian_kde(x)(t)
+			
+			TF[g].append( (t, f, d) )
 
+	print("Plotting...")
+
+	mint = min(min(t) for tf in TF.values() for (t, _, _) in tf)
+	maxt = max(max(t) for tf in TF.values() for (t, _, _) in tf)
+	maxf = max(max(f) for tf in TF.values() for (_, f, _) in tf)
+
+	for (g, tfd) in Progress()(TF.items()) :
+		plt.clf()
+		
+		for (t, f, d) in tfd :
+			is_alive = (d['median'] > 1)
+			plt.plot(t, f, ('C2' if is_alive else 'C1'))
+		
+		plt.xlim(( mint, maxt ))
+		plt.ylim((    0, maxf ))
+			
+		plt.title("Sample subset: {}_XX ({} samples)".format(g, Y.shape[axis_smpl]))
+		plt.xlabel(xlabel)
+		plt.ylabel("Frequency")
+
+		plt.savefig(
+			output_file_fig.format(
+				normalization=("sum1" if normalize else "orig"), 
+				group=g, 
+				extension="png"
+			)
+		)
